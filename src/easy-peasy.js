56cb1904829b67d4881ab40c9731dfafcd90539a
Sean Matheson <sean@ctrlplusb.com>
2018-10-22 18:03:06 +0100

    Refactoring

--- src/easy-peasy.js
+++ src/easy-peasy.js
@@ -64,7 +64,6 @@ export const createStore = (model, options = {}) => {
       const value = current[key]
       if (typeof value === 'function') {
         let handler
-
         if (value[effectSymbol]) {
           // Effect
           handler = payload =>
@@ -73,30 +72,17 @@ export const createStore = (model, options = {}) => {
             })
           handler[effectSymbol] = true
         } else {
-          // Action
-          handler = (state, payload) => {
-            const inverseChanges = []
-            let inverse = false
-            const newState = produce(
-              state,
-              draft => {
-                const handlerResult = value(draft, payload, {
-                  dispatch: references.dispatch,
-                  dispatchLocal: get(path, references.dispatch),
-                  getState: references.getState,
-                })
-                inverse = isPromise(handlerResult)
-              },
-              (_, inversePatches) => {
-                inverseChanges.push(...inversePatches)
-              },
+          // Reducer
+          handler = (state, payload) =>
+            produce(state, draft =>
+              value(draft, payload, {
+                dispatch: references.dispatch,
+                dispatchLocal: get(path, references.dispatch),
+                getState: references.getState,
+              }),
             )
-            return inverse && inverseChanges.length > 0
-              ? applyPatches(newState, inverseChanges)
-              : newState
-          }
         }
-        handler.actionName = `${path.join('.')}.${key}`
+        handler.handlerName = `${path.join('.')}.${key}`
         return { ...innerAcc, [key]: handler }
       }
       if (typeof value === 'object' && !Array.isArray(value)) {
@@ -108,7 +94,7 @@ export const createStore = (model, options = {}) => {
       return innerAcc
     }, {})
 
-  const extractActions = current =>
+  const produceActionCreators = current =>
     Object.keys(current).reduce((innerAcc, key) => {
       const value = current[key]
       if (typeof value === 'function') {
@@ -118,23 +104,23 @@ export const createStore = (model, options = {}) => {
         } else {
           action = payload =>
             references.dispatch({
-              type: value.actionName,
+              type: value.handlerName,
               payload,
             })
         }
         return { ...innerAcc, [key]: action }
       }
       if (typeof value === 'object' && !Array.isArray(value)) {
-        return { ...innerAcc, [key]: extractActions(value) }
+        return { ...innerAcc, [key]: produceActionCreators(value) }
       }
       return innerAcc
     }, {})
 
   const initialState = extractInitialState(definition)
   const handlers = extractHandlers(definition, [])
-  const actions = extractActions(handlers)
+  const actionCreators = produceActionCreators(handlers)
 
-  const extractReducer = (current, path) => {
+  const createReducers = (current, path) => {
     const handlersAtPath = Object.keys(current).reduce((acc, key) => {
       const value = current[key]
       if (typeof value === 'function' && !value[effectSymbol]) {
@@ -152,11 +138,11 @@ export const createStore = (model, options = {}) => {
     if (handlersAtPath.length > 0) {
       const nestedReducers = stateAtPath.map(key => [
         key,
-        extractReducer(current[key], [...path, key]),
+        createReducers(current[key], [...path, key]),
       ])
       const defaultState = get(path, initialState)
       return (state = defaultState, action) => {
-        const handler = handlersAtPath.find(x => x.actionName === action.type)
+        const handler = handlersAtPath.find(x => x.handlerName === action.type)
         if (handler) {
           return handler(state, action.payload)
         }
@@ -177,19 +163,19 @@ export const createStore = (model, options = {}) => {
       stateAtPath.reduce(
         (acc, key) => ({
           ...acc,
-          [key]: extractReducer(current[key], [...path, key]),
+          [key]: createReducers(current[key], [...path, key]),
         }),
         {},
       ),
     )
   }
 
+  const reducers = createReducers(handlers, [])
+
   const composeEnhancers =
     devTools && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
       ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
       : compose
-
-  const reducers = extractReducer(handlers, [])
 
   const store = reduxCreateStore(
     reducers,
@@ -197,9 +183,9 @@ export const createStore = (model, options = {}) => {
     composeEnhancers(applyMiddleware(thunk)),
   )
 
-  // attach the actions to dispatch
-  Object.keys(actions).forEach(key => {
-    store.dispatch[key] = actions[key]
+  // attach the action creators to dispatch
+  Object.keys(actionCreators).forEach(key => {
+    store.dispatch[key] = actionCreators[key]
   })
 
   references.dispatch = store.dispatch
