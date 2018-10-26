229cad84a403cd88eb167ebbf83839bcc8e4fa8d
Sean Matheson <sean@ctrlplusb.com>
2018-10-26 22:45:45 +0100

    Move to full integration with React via new Hooks APIs (#4)

---
+++ src/context.js
@@ -0,0 +1,5 @@
+import { createContext } from 'react'
+
+const StoreContext = createContext()
+
+export default StoreContext
---
+++ src/provider.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import StoreContext from './context'
+
+const StoreProvider = ({ children, store }) => (
+  <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
+)
+
+StoreProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+  store: PropTypes.object.isRequired,
+}
+
+export default StoreProvider
