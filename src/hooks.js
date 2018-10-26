3104d54c4ebb61fddae537d8bd6cb3a972abbc1c
Sean Matheson <sean@ctrlplusb.com>
2018-10-26 15:41:02 +0100

    Fixing hooks

--- src/hooks.js
+++ src/hooks.js
@@ -1,12 +1,21 @@
-import { useState, useEffect, useMemo, useContext } from 'react'
+import { useState, useEffect, useContext } from 'react'
 import EasyPeasyContext from './context'
 
 export function useStore(mapState) {
   const store = useContext(EasyPeasyContext)
   const [state, setState] = useState(mapState(store.getState()))
-  const result = useMemo(() => state, [state])
-  useEffect(() => store.subscribe(() => setState(mapState(store.getState()))))
-  return result
+  const [subscribed, setSubscribed] = useState(false)
+  useEffect(() => {
+    if (subscribed) return undefined
+    setSubscribed(true)
+    return store.subscribe(() => {
+      const newState = setState(mapState(store.getState()))
+      if (newState !== state) {
+        setState(newState)
+      }
+    })
+  })
+  return state
 }
 
 export function useAction(mapActions) {
