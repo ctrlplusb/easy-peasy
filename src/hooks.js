5fbcad682a538b81c71404ebd9fc6f15c35ee547
Sean Matheson <sean@ctrlplusb.com>
2018-10-27 22:10:32 +0100

    Adds the ability for the useStore to return an object of state

--- src/hooks.js
+++ src/hooks.js
@@ -1,18 +1,26 @@
 import { useState, useEffect, useContext } from 'react'
+import shallowEqual from 'shallowequal'
 import EasyPeasyContext from './context'
+import { isObject } from './lib'
 
 export function useStore(mapState) {
   const store = useContext(EasyPeasyContext)
-  const [state, setState] = useState(mapState(store.getState()))
+  const localState = useState(mapState(store.getState()))
   useEffect(() =>
     store.subscribe(() => {
+      const [state, setState] = localState
       const newState = mapState(store.getState())
-      if (newState !== state) {
-        setState(newState)
+      if (
+        newState === state ||
+        (isObject(newState) && isObject(state) && shallowEqual(newState, state))
+      ) {
+        // Do nothing
+        return
       }
+      setState(newState)
     }),
   )
-  return state
+  return localState[0]
 }
 export function useAction(mapActions) {
   const store = useContext(EasyPeasyContext)
