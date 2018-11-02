0aa222d118af82aaa9f36bb4ca7d8f6e40cb0781
Sean Matheson <sean@ctrlplusb.com>
2018-11-02 14:05:59 +0000

    Multi distribution builds - esm, cjs, umd, umd-min

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,16 @@
+/* eslint-disable no-param-reassign */
+
+const { uglify } = require('rollup-plugin-uglify')
 const babel = require('rollup-plugin-babel')
 const changeCase = require('change-case')
+const produce = require('immer').default
+const replace = require('rollup-plugin-replace')
+const fileSize = require('rollup-plugin-filesize')
 const packageJson = require('./package.json')
 
 process.env.BABEL_ENV = 'production'
 
-module.exports = {
+const baseConfig = {
   external: [
     'immer',
     'memoize-one',
@@ -16,12 +22,7 @@ module.exports = {
   ],
   input: 'src/index.js',
   output: {
-    file: `dist/${packageJson.name}.js`,
-    format: 'cjs',
     sourcemap: true,
-    name: changeCase
-      .titleCase(packageJson.name.replace(/-/g, ' '))
-      .replace(/ /g, ''),
   },
   plugins: [
     babel({
@@ -40,3 +41,51 @@ module.exports = {
     }),
   ],
 }
+
+const commonUMD = config =>
+  produce(config, draft => {
+    draft.output.format = 'umd'
+    draft.output.globals = {
+      'memoize-one': 'memoizeOne',
+      'prop-types': 'PropTypes',
+      'redux-thunk': 'thunk',
+      immer: 'produce',
+      react: 'React',
+      redux: 'redux',
+      shallowequal: 'shallowEqual',
+    }
+    draft.output.name = changeCase
+      .titleCase(packageJson.name.replace(/-/g, ' '))
+      .replace(/ /g, '')
+    draft.plugins.push(fileSize())
+  })
+
+module.exports = [
+  // Universal module definition (UMD) build, unminified, development
+  produce(commonUMD(baseConfig), draft => {
+    draft.output.file = `dist/${packageJson.name}.js`
+    draft.plugins = [
+      replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
+      ...draft.plugins,
+    ]
+  }),
+  // Universal module definition (UMD) build, minified, production
+  produce(commonUMD(baseConfig), draft => {
+    draft.output.file = `dist/${packageJson.name}.min.js`
+    draft.plugins = [
+      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+      ...draft.plugins,
+      uglify(),
+    ]
+  }),
+  // ESM build
+  produce(baseConfig, draft => {
+    draft.output.format = 'esm'
+    draft.output.file = `dist/${packageJson.name}.esm.js`
+  }),
+  // CJS build
+  produce(baseConfig, draft => {
+    draft.output.format = 'cjs'
+    draft.output.file = `dist/${packageJson.name}.cjs.js`
+  }),
+]
