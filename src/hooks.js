1b9472bb71afcfded70658bcf0abeac6c2c1f2e4
Sean Matheson <sean@ctrlplusb.com>
2018-11-09 06:55:53 +0000

    Adds unmount safety to the useStore hook

--- src/hooks.js
+++ src/hooks.js
@@ -9,29 +9,36 @@ export function useStore(mapState) {
   // As our effect only fires on mount and unmount it won't have the state
   // changes visible to it, therefore we use a mutable ref to track this.
   const stateRef = useRef(state)
-  useEffect(
-    () =>
-      store.subscribe(() => {
-        const newState = mapState(store.getState())
-        if (
-          newState === stateRef.current ||
-          (isStateObject(newState) &&
-            isStateObject(stateRef.current) &&
-            shallowEqual(newState, stateRef.current))
-        ) {
-          // Do nothing
-          return
+  const isMounted = useRef(true)
+  useEffect(() => {
+    const unsubscribe = store.subscribe(() => {
+      const newState = mapState(store.getState())
+      if (
+        newState === stateRef.current ||
+        (isStateObject(newState) &&
+          isStateObject(stateRef.current) &&
+          shallowEqual(newState, stateRef.current))
+      ) {
+        // Do nothing
+        return
+      }
+      stateRef.current = newState
+      // The settimeout wrap fixes a strange issue where a setState would
+      // fire but the associated hook wouldn't receive it. It's almost as
+      // if the effect was handled in a synchronous manner in some part of
+      // the React reconciliation process that ended up with it not
+      // propagating
+      setTimeout(() => {
+        if (isMounted.current) {
+          setState(newState)
         }
-        stateRef.current = newState
-        // The settimeout wrap fixes a strange issue where a setState would
-        // fire but the associated hook wouldn't receive it. It's almost as
-        // if the effect was handled in a synchronous manner in some part of
-        // the React reconciliation process that ended up with it not
-        // propagating
-        setTimeout(() => setState(newState))
-      }),
-    [],
-  )
+      })
+    })
+    return () => {
+      unsubscribe()
+      isMounted.current = false
+    }
+  }, [])
   return state
 }
 export function useAction(mapActions) {
