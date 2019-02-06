4a399368eb4652cb7c0eb9115b9bbd5935295319
Sean Matheson <sean@ctrlplusb.com>
2019-02-06 23:58:20 +0000

    Adds warning against useAction

--- src/hooks.js
+++ src/hooks.js
@@ -46,7 +46,16 @@ export function useStore(mapState, dependencies = []) {
   return state
 }
 
+let warnedAboutUseActionDeprecation = false
+
 export function useAction(mapActions) {
+  if (!warnedAboutUseActionDeprecation) {
+    warnedAboutUseActionDeprecation = true
+    // eslint-disable-next-line no-console
+    console.warn(
+      'Easy Peasy: the "useAction" hook has been deprecated and will be removed in the next major release. We recommend using the "useActions" hook instead.',
+    )
+  }
   const store = useContext(EasyPeasyContext)
   return mapActions(store.dispatch)
 }
