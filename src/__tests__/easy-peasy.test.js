fc535aaaecbbfe39f078be0c61272f8083dd3048
Sean Matheson <sean@ctrlplusb.com>
2019-02-06 20:50:50 +0000

    Adds thunk helper, and deprecates effect helper.
    (cherry picked from commit 66c83d7014f9717f26681de8420f2549580090ad)

--- src/__tests__/easy-peasy.test.js
+++ src/__tests__/easy-peasy.test.js
@@ -11,6 +11,7 @@ import {
   effect,
   reducer,
   select,
+  thunk,
   useStore,
   useAction,
   listeners,
@@ -748,6 +749,178 @@ describe('effects', () => {
       parent: ['foo'],
       path: ['foo', 'doSomething'],
     })
+  })
+})
+
+describe('thunks', () => {
+  test('dispatches an action to represent the start and end of an thunk', async () => {
+    // arrange
+    const model = {
+      foo: {
+        counter: 0,
+        increment: state => {
+          state.counter += 1
+        },
+        doSomething: thunk(actions => {
+          actions.increment()
+          return 'did something'
+        }),
+      },
+    }
+    const trackActions = trackActionsMiddleware()
+    const store = createStore(model, { middleware: [trackActions] })
+    const payload = 'hello'
+    // act
+    const actualResult = await store.dispatch.foo.doSomething(payload)
+    // assert
+    expect(trackActions.actions).toEqual([
+      { type: '@thunk.foo.doSomething(started)', payload },
+      { type: '@action.foo.increment', payload: undefined },
+      { type: '@thunk.foo.doSomething(completed)', payload },
+    ])
+    expect(actualResult).toBe('did something')
+  })
+
+  test('async', async () => {
+    // arrange
+    const model = {
+      session: {
+        user: undefined,
+        loginSucceeded: (state, payload) => {
+          state.user = payload
+        },
+        login: thunk(async (actions, payload, { getState }) => {
+          expect(payload).toEqual({
+            username: 'bob',
+            password: 'foo',
+          })
+          const user = await resolveAfter({ name: 'bob' }, 15)
+          actions.loginSucceeded(user)
+          expect(getState()).toEqual({
+            session: {
+              user: {
+                name: 'bob',
+              },
+            },
+          })
+          return 'resolved'
+        }),
+      },
+    }
+    // act
+    const store = createStore(model)
+    // act
+    const result = await store.dispatch.session.login({
+      username: 'bob',
+      password: 'foo',
+    })
+    // assert
+    expect(result).toBe('resolved')
+    expect(store.getState()).toEqual({
+      session: {
+        user: {
+          name: 'bob',
+        },
+      },
+    })
+  })
+
+  test('is always promise chainable', done => {
+    // arrange
+    const model = { doSomething: thunk(() => undefined) }
+    const store = createStore(model)
+    // act
+    store.dispatch.doSomething().then(done)
+  })
+
+  test('dispatch another branch action', async () => {
+    // arrange
+    const model = {
+      session: {
+        user: undefined,
+        login: thunk((actions, payload, { dispatch }) => {
+          dispatch.stats.incrementLoginAttempts()
+        }),
+      },
+      stats: {
+        loginAttempts: 0,
+        incrementLoginAttempts: state => {
+          state.loginAttempts += 1
+        },
+      },
+    }
+    // act
+    const store = createStore(model)
+    // act
+    await store.dispatch.session.login()
+    // assert
+    expect(store.getState()).toEqual({
+      session: {
+        user: undefined,
+      },
+      stats: {
+        loginAttempts: 1,
+      },
+    })
+  })
+
+  test('getState is exposed', async () => {
+    // arrange
+    const store = createStore({
+      count: 1,
+      doSomething: thunk((dispatch, payload, { getState }) => {
+        // assert
+        expect(getState()).toEqual({ count: 1 })
+      }),
+    })
+
+    // act
+    await store.dispatch.doSomething()
+  })
+
+  test('meta values are exposed', async () => {
+    // arrange
+    let actualMeta
+    const store = createStore({
+      foo: {
+        doSomething: thunk((dispatch, payload, { meta }) => {
+          actualMeta = meta
+        }),
+      },
+    })
+
+    // act
+    await store.dispatch.foo.doSomething()
+
+    // assert
+    expect(actualMeta).toEqual({
+      parent: ['foo'],
+      path: ['foo', 'doSomething'],
+    })
+  })
+
+  test('injections are exposed', async () => {
+    // arrange
+    const injections = { foo: 'bar' }
+    let actualInjections
+    const store = createStore(
+      {
+        foo: {
+          doSomething: thunk((dispatch, payload, helpers) => {
+            actualInjections = helpers.injections
+          }),
+        },
+      },
+      {
+        injections,
+      },
+    )
+
+    // act
+    await store.dispatch.foo.doSomething()
+
+    // assert
+    expect(actualInjections).toEqual(injections)
   })
 })
 
