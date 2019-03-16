3f981f198cd3b8ba6d83083e2dd9e489b5becd99
Sean Matheson <sean@ctrlplusb.com>
2019-03-16 20:38:15 +0000

    Fix for typescript Action type to allow no payload

--- src/__tests__/typescript/thunk.ts
+++ src/__tests__/typescript/thunk.ts
@@ -1,4 +1,4 @@
-import { Thunk, thunk } from 'easy-peasy';
+import { createStore, Thunk, thunk } from 'easy-peasy';
 
 interface Injections {
   fetch: () => Promise<void>;
@@ -6,7 +6,8 @@ interface Injections {
 
 interface AuditModel {
   logs: string[];
-  log: Thunk<AuditModel, string, Injections, StoreModel>;
+  log: Thunk<AuditModel, string, Injections, StoreModel, number>;
+  empty: Thunk<AuditModel>;
 }
 
 interface StoreModel {
@@ -29,7 +30,21 @@ const model: StoreModel = {
         dispatch.audit.log('foo');
         meta.parent.concat(['foo']);
         meta.path.concat(['foo']);
+        return 1;
       },
     ),
+    empty: thunk(() => {}),
   },
 };
+
+const store = createStore(model);
+
+store.dispatch.audit.log('foo').then(result => result + 1);
+// typings:expect-error
+store.dispatch.audit.log(1);
+// typings:expect-error
+store.dispatch.audit.log();
+
+store.dispatch.audit.empty();
+// typings:expect-error
+store.dispatch.audit.empty('foo');
