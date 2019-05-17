f758fdfaf46786f76a7f6edce64afc9f4a365dd3
Sean Matheson <sean@ctrlplusb.com>
2019-05-18 00:03:38 +0100

    Adds new derived helper

--- src/helpers.js
+++ src/helpers.js
@@ -25,6 +25,15 @@ export const action = fn => {
   return fn;
 };
 
+export const derived = (args, fn, config) => {
+  fn[derivedSymbol] = true;
+  fn[derivedConfigSymbol] = {
+    args,
+    config,
+  };
+  return fn;
+};
+
 export const listen = fn => {
   fn[listenSymbol] = true;
   return fn;
