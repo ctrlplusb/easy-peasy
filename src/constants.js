a4335b97609905d93507fc5e83691af9a4b41dc8
Sean Matheson <sean@ctrlplusb.com>
2019-05-20 13:44:03 +0100

    Renames derived helper to selector

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,6 @@
 export const actionNameSymbol = '🙈actionName🙈';
 export const actionSymbol = '🙈action🙈';
 
-export const derivedSymbol = '🙈derived🙈';
-export const derivedConfigSymbol = '🙈derivedConfig🙈';
-export const derivedStateSymbol = '🙈derivedState🙈';
-
 export const listenSymbol = '🙈listen🙈';
 
 export const metaSymbol = '🙈meta🙈';
@@ -15,5 +11,9 @@ export const selectDependenciesSymbol = '🙈selectDependencies🙈';
 export const selectImpSymbol = '🙈selectImp🙈';
 export const selectStateSymbol = '🙈selectState🙈';
 export const selectSymbol = '🙈select🙈';
+
+export const selectorSymbol = '🙈selector🙈';
+export const selectorConfigSymbol = '🙈selectorConfig🙈';
+export const selectorStateSymbol = '🙈selectorState🙈';
 
 export const thunkSymbol = '🙈thunk🙈';
