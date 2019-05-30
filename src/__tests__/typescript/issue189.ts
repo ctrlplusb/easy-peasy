166979bd87902bbacba44e28db03142264899fd8
Sean Matheson <sean@ctrlplusb.com>
2019-05-30 13:55:48 +0100

    Updates test

--- src/__tests__/typescript/issue189.ts
+++ src/__tests__/typescript/issue189.ts
@@ -6,6 +6,8 @@ interface IMyInterface {
   name: string;
 }
 
+interface Person extends IMyInterface {}
+
 interface IMyStoreModel<T extends IMyInterface> {
   myMap: {
     [id: string]: T;
@@ -13,16 +15,17 @@ interface IMyStoreModel<T extends IMyInterface> {
   values: Select<IMyStoreModel<T>, Array<T>>;
 }
 
-const generateModel = <T extends IMyInterface>() => {
-  const myStore: IMyStoreModel<T> = {
+const generateModel = <T extends IMyInterface>(): IMyStoreModel<T> => {
+  return {
     myMap: {},
     values: select(state =>
       Object.keys(state.myMap).map(key => state.myMap[key]),
     ),
   };
-  const store = createStore(myStore);
-
-  store.getState().myMap['foo'].name = 'bob';
-
-  store.getState().myMap['foo'] = { name: 'bob' } as T;
 };
+
+const store = createStore(generateModel<Person>());
+
+store.getState().myMap['foo'] = { name: 'bob' };
+
+store.getState().myMap['foo'].name = 'bob';
