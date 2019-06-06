72400b7d925a6221e544e1663f74d1a65b4bc94f
Sean Matheson <sean@ctrlplusb.com>
2019-06-06 09:26:54 +0100

    Minor refactoring on new store APIs

--- src/create-component-store.js
+++ src/create-component-store.js
@@ -16,15 +16,9 @@ export default function createComponentStore(model, config) {
         ),
       [],
     );
-    const storeRef = useRef(store);
     const previousStateRef = useRef(store.getState());
     const [currentState, setCurrentState] = useState(() => store.getState());
     useEffect(() => {
-      if (storeRef.current !== store) {
-        storeRef.current = store;
-        previousStateRef.current = store.getState();
-        setCurrentState(previousStateRef.current);
-      }
       return store.subscribe(() => {
         const nextState = store.getState();
         if (previousStateRef.current !== nextState) {
