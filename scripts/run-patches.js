c52b945db717b016d1e83c066118ab8a8911f718
Sean Matheson <sean@ctrlplusb.com>
2020-03-31 19:58:21 +0800

    Improves error handling of immer patch script

--- scripts/run-patches.js
+++ scripts/run-patches.js
@@ -1,6 +1,11 @@
 const { spawnSync } = require('child_process');
 const path = require('path');
 
+const printError = () =>
+  console.error(
+    'easy-peasy failed to patch immer. Without being able to do so you will not be able to use computed properties. Please open an issue to report this.',
+  );
+
 try {
   const immerRoot = path.dirname(require.resolve('patch-package/package.json'));
 
@@ -24,10 +29,14 @@ try {
     },
   );
 
-  console.log(String(result.stdout));
+  const resultString = String(result.stdout).trim();
+
+  if (resultString.length > 0) {
+    console.log(resultString);
+  } else {
+    printError();
+  }
 } catch (err) {
-  console.error(
-    'easy-peasy failed to patch immer. Without being able to do so you will not be able to use computed properties. Please open an issue to report this.',
-  );
+  printError();
   console.log(err.stack);
 }
