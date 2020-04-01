4e030c2a21a6cb73d9fa5a6a86d5adf2d96a6f02
Sean Matheson <sean@ctrlplusb.com>
2020-04-02 05:47:59 +0800

    Adds deprecation message to createComponentStore

--- src/create-component-store.js
+++ src/create-component-store.js
@@ -1,12 +1,28 @@
 import { useMemo, useState, useRef, useEffect } from 'react';
 import createStore from './create-store';
 
+let notified = false;
+
 /**
  * Some good references on the topic of reinitialisation:
  * - https://github.com/facebook/react/issues/14830
  */
 
 export default function createComponentStore(model, config) {
+  if (!notified) {
+    notified = true;
+    if (process.env.NODE_ENV === 'development') {
+      console.log(`easy-peasy: Deprecation Warning.
+
+You are using the createComponentStore API. This API has been deprecated and will be removed in the next major release.
+
+We recommend that you instead use the newer useLocalStore API, which is a much more flexible and concise API:
+
+    https://easy-peasy.now.sh/docs/api/use-local-store.html
+`);
+    }
+  }
+
   return function useLocalStore(initialData) {
     const store = useMemo(
       () =>
