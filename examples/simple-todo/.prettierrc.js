191012b87efc10dc10c5c98baecd3283afa994b8
Jørn Myrland <j@myrland.nu>
2022-09-16 20:15:19 +0200

    Prettier config cleanup

--- examples/simple-todo/.prettierrc.js
+++ examples/simple-todo/.prettierrc.js
@@ -6,23 +6,3 @@ module.exports = {
   tabWidth: 2,
   endOfLine: 'auto',
 };
-
-// {
-//   "arrowParens": "always",
-//   "bracketSpacing": true,
-//   "embeddedLanguageFormatting": "auto",
-//   "htmlWhitespaceSensitivity": "css",
-//   "insertPragma": false,
-//   "jsxBracketSameLine": false,
-//   "jsxSingleQuote": false,
-//   "proseWrap": "preserve",
-//   "quoteProps": "as-needed",
-//   "requirePragma": false,
-//   "semi": true,
-//   "singleQuote": false,
-//   "tabWidth": 2,
-//   "trailingComma": "es5",
-//   "useTabs": false,
-//   "vueIndentScriptAndStyle": false,
-//   "printWidth": 100
-// }
